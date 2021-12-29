58d0b28bbe9586c0fbd927c092ec2635ffa21050
mallikaamin <96690152+mallikaamin@users.noreply.github.com>
2021-12-29 15:30:04 +0530

    Mallika Assignment

---
+++ Mallika/loop.py
@@ -0,0 +1,13 @@
+#Program to print the tables
+data=[1,2,3,4,5,6,7,8,9,10]
+number=int(input("enter the table number"))
+print("muli:",number)
+for i in data:
+    print(number*i)
+
+
+#print 1 to 5 using while loop
+i=1
+while i<6:
+    print(i)
+    i+=1

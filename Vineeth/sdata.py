a3833bce77915e6676255dfa727304adfaa72f6d
VINEETHPOOJARY <96689144+VINEETHPOOJARY@users.noreply.github.com>
2021-12-29 15:19:12 +0530

    Vineeth

---
+++ Vineeth/sdata.py
@@ -0,0 +1,92 @@
+#Empty list
+list=[]
+print(list)
+
+#List with mixed data types
+list=[1,"Hello",3.5]
+print(list)
+print(type(list))
+
+#list with integers
+list=[1,2,3]
+print(list)
+print(type(list))
+
+#list accessing
+list=[3,5,2,7,8,5]
+print(list[:6])
+print(type(list))
+
+#list accessing
+list=[3,5,2,7,8,5]
+print(list[0:4])
+print(type(list))
+
+#set
+a={5,2,3,1,4}
+print("a=",a)
+print(type(a))
+
+#string using single quote
+string='Welcome to Python'
+print(string)
+print(type(string))
+
+#string using Double Quote
+string="I'm vaibhav"
+print(string)
+print(type(string))
+
+#string in multiple line
+string="""My
+favorite
+language"""
+print(string)
+print(type(string))
+
+tuple=()
+print("empty tuple---",tuple)
+print(type(tuple))
+
+tuple=('Python','Program')
+print("Tuple using list--")
+print(tuple)
+print(type(tuple))
+
+Tuple=[1,2,3,4,5]
+print("tuple using list")
+print(Tuple)
+print(type(tuple))
+
+tuple=type('Python')
+print("\ntuple usingfunction")
+print(tuple)
+
+#Dictionary
+dic={'name':'vaibhav','age':20}
+print(dic['name'])
+print(dic['age'])
+#error
+print(dic['address'])
+
+
+#list with integers
+list=[1,2,3]
+print(list)
+print(type(list))
+
+#list accessing
+list=[3,5,2,7,8,5]
+print(list[:6])
+print(type(list))
+
+#list accessing
+list=[3,5,2,7,8,5]
+print(list[0:4])
+print(type(list))
+
+#set
+a={5,2,3,1,4}
+print("a=",a)
+print(type(a))
+

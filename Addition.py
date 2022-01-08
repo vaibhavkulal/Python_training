9f71178263aa9f81857563e4450c15d456bec80f
Hithaish <96730035+163CS19013@users.noreply.github.com>
2022-01-08 18:36:52 +0530

    Add files via upload

---
+++ Addition.py
@@ -0,0 +1,15 @@
+#  input floating numbers 
+num1= float(input('\nEnter first float number: '))
+num2=float(input('\nEnter second float number:'))
+
+  # Input Integer type numbers
+num3=int(input('\nEnter first integer number:'))
+num4=int(input('\nEnter the second integer number:'))
+  # Add two numbers
+sum=num1+num2
+
+sum1=num3+num4
+# Display the sum
+
+print('\nThe sum of float numbers {0} and {1} is {2}'.format(num1,num2,sum)) 
+print('\nThe sum of integer numbers {0}and {1} ts {2}'.format(num3,num4,sum1))

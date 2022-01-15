a0337b7602130a3e5db03ec523a368f5939e168a
Vikas7C <96731855+Vikas7C@users.noreply.github.com>
2022-01-15 10:24:41 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,66 @@
+ 
+ ​# append  
+ ​computer= ['CPU', 'Keyborad', 'Mouse'] 
+ ​print(computer) 
+ ​computer.append('Monitor') 
+ ​print(computer,"\n") 
+  
+ ​# extend 
+ ​prime_number=[1,2,3] 
+ ​print('\n',prime_number) 
+ ​number=[1,4] 
+ ​print(number) 
+ ​number.extend(prime_number) 
+ ​print(number,"\n") 
+  
+ ​# "insert"  
+ ​vowel=['a','e','i','u'] 
+ ​print(vowel) 
+ ​vowel.insert(3,'o') 
+ ​print(vowel,'\n') 
+  
+ ​# "Remove" 
+ ​num=[2,3,4,5,6] 
+ ​print('\n',num) 
+ ​num.remove(5) 
+ ​print(num,'\n') 
+  
+ ​# "pop"  
+ ​list=[2,3,4,5,6,7,8] 
+ ​print(list) 
+ ​list.pop(2) 
+ ​print("popped 4 index is 2",list,"\n") 
+  
+ ​# "clear"  
+ ​print(list) 
+ ​list.clear() 
+ ​print(list,"\n") 
+  
+ ​# "Index"  
+ ​print(num) 
+ ​index = num.index(6) 
+ ​print("Index of 6 is =",index) 
+ ​print(num,"\n") 
+  
+ ​# "count" 
+ ​l=[2,3,3,3,5,6,3,7] 
+ ​print(l) 
+ ​cou=l.count(3) 
+ ​print("number times 3 is appeared",cou,"\n") 
+  
+ ​# "sort" 
+ ​element=[4,2,7,6,1,8] 
+ ​print(element) 
+ ​element.sort() 
+ ​print(element,"\n") 
+  
+ ​# "reverse"  
+ ​print(element) 
+ ​element.reverse() 
+ ​print(element,"\n") 
+  
+ ​# "copy" 
+ ​name=["Vaibhav"] 
+ ​my_name=name.copy() 
+ ​print(name) 
+ ​print(my_name,"\n")
